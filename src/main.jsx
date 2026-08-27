8134ef9a228645b95b57d598268ec8c08e1979a6
HafenConsol <info.hafen@proton.me>
2026-08-27 13:11:46 -0300

    feat: load isolated Aves biodiversity theme

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles/babylon-worlds.css'
 import './styles/contact-icons-only.css'
 import './styles/white-biodiversity.css'
 import './styles/babylon-bright-home.css'
+import './styles/aves-white-biodiversity.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
