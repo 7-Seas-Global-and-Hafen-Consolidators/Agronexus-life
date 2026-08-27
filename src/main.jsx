17d28b41dad9c05ce0730d85897e967b56a890ce
HafenConsol <info.hafen@proton.me>
2026-08-27 13:20:48 -0300

    feat: load isolated marine visual skin

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './styles/contact-icons-only.css'
 import './styles/white-biodiversity.css'
 import './styles/babylon-bright-home.css'
 import './styles/aves-white-biodiversity.css'
+import './styles/marine-white-biodiversity.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
